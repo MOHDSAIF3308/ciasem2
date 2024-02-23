1d75fe9b46bacf6154578501d4e4d5f24d3e4cca
md.saif <mdsaif92336@gmail.com>
2024-02-24 00:21:48 +0530

    Your commit message here

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from 'react';
+import NavBar from './components/NavBar';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-     {/*  <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>saif </h1>
-      </header>
+    <div className="app">
+      <NavBar />
+      <h1>Hello, React App!</h1>
+      {/* Add more components or content as needed */}
     </div>
   );
-}
+};
 
 export default App;
